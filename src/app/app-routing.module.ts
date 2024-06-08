7394ad8f0b74b6cad0427ccd2fc5003b82088ef2
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-06-07 20:28:24 -1200

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path:'login', component:LoginComponent},
+  { path:'welcom',component:WelcomeComponent}
+
+  // { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  // { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
